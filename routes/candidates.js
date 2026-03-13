be96bf83395f2f66c818747216ef9be2778f15ea
Jose Perez <jotangamers@gmail.com>
2026-03-12 20:50:17 -0400

    feat: implement dynamic Prisma POST controller for candidates

--- routes/candidates.js
+++ routes/candidates.js
@@ -4,7 +4,7 @@ import prisma from "../lib/prisma.js";
 const router = express.Router();
 
 router.post('/evaluate', async (req, res) => {
-    const payload = req.body;
+    const payload = req.body; // JSON container aplied in index.js
 
     if (!payload) {
         res.status(400).json({ error: "Error to receive the data" });
@@ -14,9 +14,38 @@ router.post('/evaluate', async (req, res) => {
     try {
         const newUser = await prisma.user.create({
             data: {
-                email: "payload.email",
-                password: "12345",
+                email: payload.email,
+                password: payload.password,
+
+                jobRequirement: {
+                    create: {
+                        role: payload.role,
+                        yearsOfExperience: payload.yearsOfExperience,
+                        technicalSkills: payload.technicalSkills,
+                        optionalTechnicalSkills: payload.optionalTechnicalSkills,
+                        softSkills: payload.softSkills,
+                        description: payload.description,
+                        education: payload.education,
+                        languages: payload.languages
+                    }
+                },
+
+                candidate: {
+                    create: {
+                        fullName: payload.fullName,
+                        role: payload.role,
+                        email: payload.email, // Must be unique
+                        fileUrl: payload.fileUrl,
+                        technicalSkills: payload.technicalSkills,
+                        softSkills: payload.softSkills,
+                        personalProject: payload.personalProject,
+                        phoneNumber: payload.phoneNumber, // Must be string
+                        education: payload.education,
+                        languages: payload.languages
+                    }
+                }
             },
+
         });
 
         console.log("Database write succesful:", newUser.email);
