bbfcfe5555bb7c42ee681362a7e60961ea7281a8
Jose Perez <jotangamers@gmail.com>
2026-03-19 21:35:34 -0400

    refactor(api): reconstruct project implementation to transition from nested creation to a modular resource-based architecture

--- src/routes/candidates.js
+++ src/routes/candidates.js
@@ -1,55 +1,42 @@
 import express from "express";
 import prisma from "../lib/prisma.js";
 import upload from '../middlewares/multerConfig.js';
-import { error } from "node:console";
-import { buffer } from "node:stream/consumers";
 
 const router = express.Router();
 
-// Giving the JSON to Front End
-router.get("/", async (req, res) => {
-    try {
-        // Selecting all the object
-        const allCandidates = await prisma.user.findMany({
-            include: {
-                jobRequirement: true,
-                candidate: true,
-            }
-        });
-
-        return res.status(200).json(allCandidates);
-    } catch (error) {
-        console.error("Database error", error)
-        return res.status(500).json({ error: "Failed to fetch candidates" });
-    }
-});
-
-router.get("/:id", async (req, res) => {
-    try {
-        const idSearch = parseInt(req.params.id);
-        const allcandidate = await prisma.user.findMany({
-            include: {
-                jobRequirement: true,
-                candidate: true
-            }
-        });
-
-        if (isNaN(idSearch)) {
-            res.status(400).json({ error: "We just accept number values" });
-            return;
-        }
-
-        const idCandidates = allcandidate.find(c => c.id === idSearch)
-        return idCandidates ? res.json((idCandidates)) : res.status(404).json({ error: "Candidate not found" });
-    } catch (error) {
-        console.error("Database error", error);
-        return res.status(500).json({ error: "Failed to fetch ID candidate" });
-    }
-});
-
 // Sending the JSON to the Database
 
-router.post('/evaluate', async (req, res) => {
+router.post('/user', async (req, res) => {
+    const payload = req.body;
+
+    try {
+        if (!payload) {
+            res.status(400).json({ error: "Error to receive the data" });
+            return
+        }
+
+        const newUser = await prisma.user.create({
+            data: {
+                email: payload.email,
+                password: payload.password
+            }
+        });
+
+        console.log("Database write succesful:", newUser);
+
+        return res.status(201).json({
+            message: 'Data receive successfully',
+            userId: newUser.id
+        });
+
+    } catch (error) {
+        console.error("Database error", error);
+        return res.status(500).json({ error: "Internal server error during database operation" });
+    }
+
+});
+
+router.post('/position', async (req, res) => {
     const payload = req.body; // JSON container aplied in index.js
 
     if (!payload) {
@@ -58,55 +45,24 @@ router.post('/evaluate', async (req, res) => {
     }
 
     try {
-        const newUser = await prisma.user.create({
+        const newPosition = await prisma.position.create({
             data: {
-                email: payload.email,
-                password: payload.password,
-
-                jobRequirement: {
-                    create: {
-                        role: payload.role,
-                        yearsOfExperience: payload.yearsOfExperience,
-                        technicalSkills: payload.technicalSkills,
-                        optionalTechnicalSkills: payload.optionalTechnicalSkills,
-                        softSkills: payload.softSkills,
-                        description: payload.description,
-                        education: payload.education,
-                        languages: payload.languages
-                    }
-                },
-            },
-
-            include: { jobRequirement: true }
+                role: payload.role,
+                yearsOfExperience: payload.yearsOfExperience,
+                technicalSkills: payload.technicalSkills,
+                optionalTechnicalSkills: payload.optionalTechnicalSkills,
+                softSkills: payload.softSkills,
+                description: payload.description,
+                education: payload.education,
+                languages: payload.languages,
+                userId: payload.userId
+            }
         });
 
-        console.log("Database write succesful:", newUser.email);
+        console.log("Database write succesful:", newPosition.role);
 
         return res.status(201).json({ message: 'Data receive successfully' });
 
-    } catch (error) {
-        console.error("Database error", error);
-        return res.status(500).json({ error: "Internal server error during database operation" });
-    }
-});
-
-router.post('/upload', upload.single('pdf'), (req, res) => {
-    if (!req.file) {
-        return res.status(400).json({ error: "No PDF uploaded" })
-    }
-
-    try {
-        const isPdfSignatureValid = (buffer) => {
-            const magicSignature = buffer.toString('hex', 0, 4);
-            return magicSignature === '25504446';
-        };
-
-        console.log("The PDF signature is valid");
-
-        return res.status(200).json({
-            message: "PDF upload successfully to Cloudinary!",
-            fileData: req.file
-        });
     } catch (error) {
         console.error("Database error", error);
         return res.status(500).json({ error: "Internal server error during database operation" });
