88e3d8febca29a306d0a5703b7fbd4e3f83f67e6
Jose Perez <jotangamers@gmail.com>
2026-03-26 21:53:09 -0400

    feat(vacancies): add initial router skeleton

---
+++ src/routes/vacancies.js
@@ -0,0 +1,14 @@
+import express from "express";
+import prisma from "../lib/prisma.js"
+
+const router = express.Router();
+
+router.get('/', async (req, res) => { });
+
+router.post('/', async (req, res) => { });
+
+router.put('/:id', async (req, res) => { });
+
+router.delete('/:id', async (req, res) => { });
+
+export default router;
