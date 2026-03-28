a156971c13e6c97f4a10de250af99d85af31b84a
Jose Perez <jotangamers@gmail.com>
2026-03-27 23:53:53 -0400

    feat(vacancies): implement POST route with date conversion

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import users from "./routes/users.js"
 import positions from "./routes/positions.js"
 import uploads from "./routes/uploads.js"
+import vacancies from "./routes/vacancies.js"
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use("/api/users/", users);
 app.use("/api/positions/", positions);
 app.use("/api/uploads/", uploads);
+app.use("/api/vacancies/", vacancies);
 
 app.listen(port, () => {
     console.log(`The server is running in http://localhost:${port}/api/users`);
--- src/routes/vacancies.js
+++ src/routes/vacancies.js
@@ -5,7 +5,33 @@ const router = express.Router();
 
 router.get('/', async (req, res) => { });
 
-router.post('/', async (req, res) => { });
+router.post('/', async (req, res) => {
+    const payload = req.body;
+    if (Object.keys(payload).length === 0) {
+        return res.status(400).json({ error: "Error to receive the data" });
+    }
+
+    try {
+        const newVacancies = await prisma.vacancy.create({
+            data: {
+                title: payload.title,
+                openDate: new Date(payload.openDate),
+                closeDate: new Date(payload.closeDate),
+                positionId: payload.positionId
+            },
+
+            include: {
+                matchResults: true
+            }
+        });
+
+        console.log("Database write sucessful:", newVacancies);
+        return res.status(200).json({ message: 'Data receive successfully' });
+    } catch (error) {
+        console.error("Database error", error);
+        return res.status(500).json({ error: "Internal server error during database operation" });
+    }
+});
 
 router.put('/:id', async (req, res) => { });
 
