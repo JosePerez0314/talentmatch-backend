15414eead64c4783d4013d64de780174fa4414e5
Jose Perez <jotangamers@gmail.com>
2026-03-28 17:24:18 -0400

    feat(vacancies): add GET endpoint to fetch a single vacancy

--- src/routes/vacancies.js
+++ src/routes/vacancies.js
@@ -1,5 +1,6 @@
 import express from "express";
 import prisma from "../lib/prisma.js"
+import { error } from "console";
 
 const router = express.Router();
 
@@ -46,7 +47,22 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.put('/:id', async (req, res) => { });
+router.get('/:id', async (req, res) => {
+    const idSearch = parseInt(req.params.id);
+
+    if (isNaN(idSearch)) return res.status(400).json({ error: "Vancancies IDs only accept numeric values" });
+
+    try {
+        const vacancy = await prisma.vacancy.findUnique({
+            where: { id: idSearch }
+        })
+
+        return vacancy ? res.status(200).json(vacancy) : res.status(404).json({ error: "Vacancy not found" });
+    } catch (error) {
+        console.error("Database error", error);
+        return res.status(500).json({ error: "Internal server error during database operation" });
+    }
+});
 
 router.delete('/:id', async (req, res) => { });
 
