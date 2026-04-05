68a7a4e1777147fb481abfe8acf1c71a14c4390d
Jose Perez <jotangamers@gmail.com>
2026-04-05 17:35:07 -0400

    build(package): set node engine to 20.x

--- src/middlewares/cloudinaryConfig.js
+++
@@ -1,6 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-// Auto configure with .env file CLOUDINARY_URL="cloudinary://<your_api_key>:<your_api_secret>@<your_cloud_name>"
-cloudinary.config(true);
-
-export default cloudinary;
